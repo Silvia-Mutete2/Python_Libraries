94b8e43c0beb07ba4f5b136447415e950e589bc8
Silvia Mutete <silviamutetekisoi@gmail.com>
2025-09-02 12:12:19 +0300

    worked with pillows

--- Pillowz.py
+++ Pillowz.py
@@ -1,7 +1,8 @@
 from PIL import Image  # Ask for the artisan's specific skill
+from PIL import ImageFilter
 
 # Open an image file
-image = Image.open('path_to_your_image.jpg')
+image = Image.open('pexels-pixabay-265072.jpg')
 
 # The artisan works their magic: rotate and convert to grayscale
 rotated_image = image.rotate(45)
@@ -9,3 +10,12 @@ bw_image = rotated_image.convert('L')
 
 # Save the new creation
 bw_image.save('my_artistic_image.jpg')
+
+#filter the image
+filtered_image = image.filter(ImageFilter.BLUR)
+filtered_image.save('my_filtered_image.jpg')
+
+#blur the image
+blurred_image = image.filter(ImageFilter.GaussianBlur(5))
+blurred_image.save('my_blurred_image.jpg')
+
